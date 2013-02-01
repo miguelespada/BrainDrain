32f7cae8bc53e452e4c56543aa9fe594d012c114
miguel espada <miguel.valero.espada@gmail.com>
2013-02-01 11:15:31 +0100

    add some info

--- backend/insert.php
+++ backend/insert.php
@@ -1,5 +1,4 @@
 <?php
-	$bd = new SQLite3('brains.db');
 	
 	$origen = $_POST["origen"];
 	$destino = $_POST["destino"];
@@ -22,12 +21,13 @@
 	echo '<br>[PHP] fields: '.$fields;
 	echo '<br>[PHP] values: '.$values;
 
+	$t0 = microtime();
+	$bd = new SQLite3('brains.db');
 	$query = "INSERT INTO Brains ".$fields."VALUES ".$values;
-	
 	$results = $bd->exec($query);
 
 	if($results)
-		echo "<br> [SQL] One row inserted";
+		echo "<br> [SQL] One row inserted ".((microtime() - $t0) * 1000) ." ms" ;
 	else
 		echo "<br> [SQL] Something wrong happend";
 
